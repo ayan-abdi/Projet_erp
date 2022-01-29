da5122cab013cdfad5f0a698a4c475b9fae90bfa
delvauxo <delvauxo@outlook.com>
2022-01-29 18:22:00 +0100

    Supplier first part class

---
+++ public/models/Supplier.js
@@ -0,0 +1,14 @@
+class Supplier {
+
+    constructor(id, name, addres, product_id, total_purchases) {
+        this.id = id
+        this.name = name
+        this.addres = addres
+        this.product_id = product_id
+        this.total_purchases = total_purchases
+    }
+
+    
+}
+
+export default Supplier
